2e3c795f058b4d5a5b1da0e39fab324e1bb12281
shbodake <sayalibodake28@gmail.com>
2021-10-31 20:17:51 +0530

    Added: Gambler player won or loss game

--- src/com/blp/gamblingsimulation/GamblingSimulationProblem.java
+++ src/com/blp/gamblingsimulation/GamblingSimulationProblem.java
@@ -1,8 +1,19 @@
 package com.blp.gamblingsimulation;
-
+import java.util.Random;
 public class GamblingSimulationProblem {
     public static void main(String[] args) {
         int stake_of_Gambler= 100;
         int bet_for_Game = 1;
+        Random ran = new Random();
+        int i = ran.nextInt(2);
+        if (i==0){
+            System.out.println("Player won the game");
+            stake_of_Gambler=stake_of_Gambler+bet_for_Game;
+        }
+        else{
+            System.out.println("Player loss the Game");
+            stake_of_Gambler=stake_of_Gambler-bet_for_Game;
+        }
+
     }
 }
