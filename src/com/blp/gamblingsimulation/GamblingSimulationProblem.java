72c42483f0f5d150e024a818543da23748ae2d59
shbodake <sayalibodake28@gmail.com>
2021-10-31 20:36:51 +0530

    Added: Gambler player know total amount win or loss

--- src/com/blp/gamblingsimulation/GamblingSimulationProblem.java
+++ src/com/blp/gamblingsimulation/GamblingSimulationProblem.java
@@ -5,18 +5,35 @@ public class GamblingSimulationProblem {
         int stake_of_Gambler = 100;
         int bet_for_Game = 1;
         int count = 1;
-        while (stake_of_Gambler <= 149 && stake_of_Gambler >= 51) {
-            Random ran = new Random();
-            int i = ran.nextInt(2);
-            if (i == 0) {
-                stake_of_Gambler = stake_of_Gambler + bet_for_Game;
-                count++;
-            } else {
-                stake_of_Gambler = stake_of_Gambler - bet_for_Game;
-                count++;
+        int win =0;
+        int loss =0;
+        int net_stake_after_30_Days=0;
+        for (int k = 1; k <= 20; k++) {
+            stake_of_Gambler = 100;
+            while (stake_of_Gambler <= 149 && stake_of_Gambler >= 51) {
+                Random ran = new Random();
+                int i = ran.nextInt(2);
+                if (i == 0) {
+                    stake_of_Gambler = stake_of_Gambler + bet_for_Game;
+                    count++;
+                } else {
+                    stake_of_Gambler = stake_of_Gambler - bet_for_Game;
+                    count++;
+                }
+                if (stake_of_Gambler == 150) {
+                    win++;
+
+                } else if (stake_of_Gambler == 50) {
+                    loss++;
+                }
             }
+            net_stake_after_30_Days = (win * 50) - (loss * 50);
+        }
             System.out.println("The amount at Gamer after end of Game : " + stake_of_Gambler);
             System.out.println("The No of Games Played by Gamer : " + count);
+            System.out.println("The no of days Gamer win : "+win);
+            System.out.println("The no of days Gamer loss : "+loss);
+            System.out.println("The amount gain(+) or loss(-) after 20 days : "+net_stake_after_30_Days);
         }
     }
-}
+
