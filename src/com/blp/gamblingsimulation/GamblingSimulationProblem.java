596e658a54c902bdd8e9a8a569d7b49f002ec7b5
shbodake <sayalibodake28@gmail.com>
2021-10-31 20:09:41 +0530

    Added: Gambler start with stake and bet

--- src/com/blp/gamblingsimulation/GamblingSimulationProblem.java
+++ src/com/blp/gamblingsimulation/GamblingSimulationProblem.java
@@ -1,4 +1,8 @@
 package com.blp.gamblingsimulation;
 
 public class GamblingSimulationProblem {
+    public static void main(String[] args) {
+        int stake_of_Gambler= 100;
+        int bet_for_Game = 1;
+    }
 }
